a2ad16b5498da4d7798f0a0ce10a0402e6031c8b
Igor Polousov <igorpolousov@gmail.com>
2025-10-01 17:18:22 +0300

    + http downloader

---
+++ ToDo_VIPER/Resources/DataDownloader/HTTPDataDownloader.swift
@@ -0,0 +1,22 @@
+//
+//  HTTPDataDownloader.swift
+//  ToDo_VIPER
+//
+//  Created by Igor Polousov on 01.10.2025.
+//
+
+import Foundation
+
+let validStatus = 200 ... 299
+
+protocol HTTPDataDownloader {
+    func httpData(from url: URL) async throws -> Data
+}
+
+extension URLSession: HTTPDataDownloader {
+    func httpData(from url: URL) async throws -> Data {
+        guard let (data,response) = try await self.data(from: url, delegate: nil) as? (Data, HTTPURLResponse),
+              validStatus.contains(response.statusCode) else { throw ErrorHandler.networkError }
+        return data
+    }
+}
