902e5e4f42f3deac330cbd4f7f596e2def3f0989
Igor Polousov <igorpolousov@gmail.com>
2025-10-01 10:07:28 +0300

    Initial Commit

---
+++ ToDo_VIPER/ToDo_VIPERApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_VIPERApp.swift
+//  ToDo_VIPER
+//
+//  Created by Igor Polousov on 01.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_VIPERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
