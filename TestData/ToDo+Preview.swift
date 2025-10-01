6767e584609ab0e6b88512e814af79c2930a71c4
Igor Polousov <igorpolousov@gmail.com>
2025-10-01 16:17:38 +0300

    + file with sample preview content for building view

---
+++ TestData/ToDo+Preview.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo+Preview.swift
+//  ToDo_VIPER
+//
+//  Created by Igor Polousov on 01.10.2025.
+//
+
+import Foundation
+
+extension ToDo {
+    static var preview: ToDo {
+        var toDo = ToDo(id: 1, toDo: "Do something nice for someone you care about", completed: false, userId: 152)
+        return toDo
+    }
+}
