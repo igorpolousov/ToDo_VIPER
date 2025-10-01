f8c93cc722f3de00a758474625521f201a1728c9
Igor Polousov <igorpolousov@gmail.com>
2025-10-01 10:39:26 +0300

    + colors ext

--- ToDo_VIPER/Resources/Theme/Theme.swift
+++ ToDo_VIPER/Resources/Theme/Theme.swift
@@ -8,11 +8,12 @@
 import SwiftUI
 
 extension Font {
-    public static let headerFont: Font = (Font.custom("SF-pro", size: 34))
-    public static let mainFont: Font = (Font.custom("SF-pro", size: 16))
-    public static let secondaryFont: Font = (Font.custom("SFProText", size: 12))
+    public static let headerFont: Font = Font.custom("SF-pro", size: 34)
+    public static let mainFont: Font = Font.custom("SF-pro", size: 16)
+    public static let secondaryFont: Font = Font.custom("SFProText", size: 12)
 }
 
 extension Color {
-    
+    public static let themeAccent: Color = Color("AccentColor")
+    public static let themeGray: Color = Color("TaskCircleColor")
 }
